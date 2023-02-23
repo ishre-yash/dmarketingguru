08f37b2abd70bead26594afa116a5b3de0d55f2b
Shre Yash <iamshreyash.in@gmail.com>
2023-02-23 11:26:21 +0530

    update

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'My personal website.';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = "#1 Ranked Digital Marketing Agency in Haridwar";
+export const SITE_DESCRIPTION =
+  "DMarketingGuru was established last year in November 2021 by our founder Mr. Ankur Rawat who has 15+ years of experience in the paid media industry, he came up with the …";
